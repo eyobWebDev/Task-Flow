ed64042da1fe2e733b06c36ed7710992303a89cc
Eyob Tilahun <eyobdevmedia@gmail.com>
2025-08-14 04:33:22 +0300

    added folder structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { useEffect } from 'react'
-import { useContext } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [email, setEmail] = useState()
 
   return (
     <>
