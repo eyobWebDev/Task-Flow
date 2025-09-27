107ec50341f00535a4245f4c8250a610d6f2d09b
Eyob Tilahun <eyobdevmedia@gmail.com>
2025-09-27 09:15:14 +0300

    addedmongoose cluster url

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose"
 import {config } from "dotenv"
 config()
 
-//const uri = process.env.NODE_ENV == "development" ? process.env.LOCAL_MONGODB_URI : process.env.MONGODB_URI
+const uri =
+  process.env.NODE_ENV === "development"
+    ? process.env.LOCAL_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
+        const conn = await mongoose.connect(uri)
         console.log(`mongo db connected on ${conn.connection.host}`)
     } catch (e) {
         console.log("Error while connecting: "+ e)
