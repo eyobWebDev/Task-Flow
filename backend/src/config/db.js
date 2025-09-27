acd9feceac07ae024e47dfe37b796a005c09a02a
Eyob Tilahun <eyobdevmedia@gmail.com>
2025-09-27 09:04:06 +0300

    added production level db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,7 +6,7 @@ config()
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.LOCAL_MONGODB_URI)
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
         console.log(`mongo db connected on ${conn.connection.host}`)
     } catch (e) {
         console.log("Error while connecting: "+ e)
