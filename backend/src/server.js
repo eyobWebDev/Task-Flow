0685af3fdf9569da5fe230eea95618fc33d7d0f6
Eyob Tilahun <eyobdevmedia@gmail.com>
2025-09-27 09:08:54 +0300

    added the live urls for base api url

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ import taskRouter from "./routes/task.routes.js"
 config()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://task-flow-hiy531nmx-full-stack-web-dev.vercel.app/",
     credentials: true
 }))
 app.use(cookieParser())
