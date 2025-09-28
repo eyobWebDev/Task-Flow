fc68accf93dfacdfa8705909206c3c43eac8fd0c
Eyob Tilahun <eyobdevmedia@gmail.com>
2025-09-28 19:09:21 +0300

    fixed cors issue adding the deployed backend url

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const Axios = axios.create({
-    baseURL: import.meta.env.MODE == "development" ? "http://localhost:5001" : "https://mernchatapp-qlo9.onrender.com",
+    baseURL: import.meta.env.MODE == "development" ? "http://localhost:5001" : "https://task-flow-aarm.onrender.com",
     withCredentials: true
 })
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_API_URL = ""
+export const BASE_API_URL = "https://task-flow-aarm.onrender.com"
 export const NODE_ENV = "development"
