81b73a9d2ac29fe20bea5e94b31eecb547a0e4ba
Eyob Tilahun <eyobdevmedia@gmail.com>
2025-10-05 16:45:27 +0300

    added default spin for when getting project list

--- frontend/src/features/project/ProjectsListView.jsx
+++ frontend/src/features/project/ProjectsListView.jsx
@@ -6,12 +6,16 @@ import StackedAvatar from "@/widget/feature/StackedAvatar";
 import { FolderOpen, MoreHorizontal } from "lucide-react";
 import CreateProjectDialog from "./CreateProjectDialog";
 import { Button } from "@/components/ui/button";
+import SpinAnimate from "@/widget/SpinAnimate";
 
 
 export default function ProjectsListView(){
-    const {projects} = useProjectStore()
+    const {projects, isGettingProject} = useProjectStore()
 
     console.log("projects", projects);
+    if(isGettingProject){
+        return <SpinAnimate text={`Getting allthe projects...`} />
+    }
 
     return <div className="lg:pl-8 lg:pt-5 pt-2 grid lg:grid-cols-2 grid-cols-1 lg:pr-10 pl-2 gap-5 pr-2">
         {/* <ProjectBox project={{username: "username", description: "Descriton - detailed descriptiom.", 
